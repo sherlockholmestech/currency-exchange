cdb554e5a3fa291629f89c6296febdfad589394d
Sherlock Holmes <sherlockholmesxp@gmail.com>
2024-12-16 22:51:31 +0800

    shows exchange rate

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -48,8 +48,9 @@
 			<Input type="text" id="base" placeholder="100" bind:value={baseValue} />
 			<p class="text-muted-foreground text-sm">The currency to convert form (Base Currency).</p>
 		</div>
-		<div class="flex justify-center items-center content-center">
+		<div class="flex justify-center items-center flex-col">
 			<Button onclick={swapCurrency}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.7.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M32 96l320 0 0-64c0-12.9 7.8-24.6 19.8-29.6s25.7-2.2 34.9 6.9l96 96c6 6 9.4 14.1 9.4 22.6s-3.4 16.6-9.4 22.6l-96 96c-9.2 9.2-22.9 11.9-34.9 6.9s-19.8-16.6-19.8-29.6l0-64L32 160c-17.7 0-32-14.3-32-32s14.3-32 32-32zM480 352c17.7 0 32 14.3 32 32s-14.3 32-32 32l-320 0 0 64c0 12.9-7.8 24.6-19.8 29.6s-25.7 2.2-34.9-6.9l-96-96c-6-6-9.4-14.1-9.4-22.6s3.4-16.6 9.4-22.6l96-96c9.2-9.2 22.9-11.9 34.9-6.9s19.8 16.6 19.8 29.6l0 64 320 0z"/></svg></Button>
+			<p class="text-muted-foreground text-sm pt-3">1 {currencyData.currencyBase} = {exchangeRate.unit * exchangeRate.mid} {currencyData.currencyTarget}</p>
 		</div>
 		<div class="flex w-full max-w-sm flex-col gap-1.5">
 			<ComboboxTarget {currencyData}/>
